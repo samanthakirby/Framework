5f7174e76ede86f278500a554a4e3168e34d8301
Samantha Kirby <49468075+samanthakirby@users.noreply.github.com>
2022-11-14 15:59:49 +1300

    Add Junit reporting

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,17 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   watchForFileChanges: false,
+  trashAssetsBeforeRuns: true,
+  screenshotOnRunFailure: true,
+  video: false,
+  videoUploadOnPasses: false,
+  chromeWebSecurity: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
   e2e: {
+    experimentalSessionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}"
